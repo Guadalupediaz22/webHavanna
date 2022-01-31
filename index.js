5146490435bc225deb5ab7a2986982bcaab902fc
Guadalupe Diaz <diazmariaguadalupe946@gmail.com>
2022-01-31 20:56:26 -0300

    primer commit

---
+++ index.js
@@ -0,0 +1,19 @@
+//hamburguesa
+const hamburIcon = document.querySelector("#burger");
+const navbarmenu = document.querySelector("#nav-links");
+
+hamburIcon.addEventListener("click", () => {
+  navbarmenu.classList.toggle(is - active);
+});
+//Modal
+// const registrarse = document.querySelector("#regis");
+// const modalbg = document.querySelector(".modal-background");
+// const modal = document.querySelector(".modal");
+
+// registrarse.addEventListener("click", () => {
+//   modal.classList.add("is-active");
+// });
+
+// modalbg.addEventListener("click", () => {
+//   modal.classList.remove("is-active");
+// });
